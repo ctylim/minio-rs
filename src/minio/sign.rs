3e9b68e501924b0a188c2ae367ed17519feeab81
Kotaro Shikata <lim.0.ctyl@gmail.com>
2021-01-22 19:30:28 +0900

    upgrade ring

--- src/minio/sign.rs
+++ src/minio/sign.rs
@@ -144,7 +144,7 @@ fn string_to_sign(ts: &Tm, scope: &str, canonical_request: &str) -> String {
 }
 
 fn hmac_sha256(msg: &str, key: &[u8]) -> hmac::Signature {
-    let key = hmac::SigningKey::new(&digest::SHA256, key);
+    let key = hmac::SigningKey::new(hmac::HMAC_SHA256, key);
     hmac::sign(&key, msg.as_bytes())
 }
 
